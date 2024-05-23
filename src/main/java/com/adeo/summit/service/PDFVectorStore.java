bb4b29e2e1bb12140546ae69d82f61883403aa3b
20014643 <marcel@marquez.fr>
2024-05-23 22:00:33 +0200

    add PGVector store

--- src/main/java/com/adeo/summit/service/PDFVectorStore.java
+++ src/main/java/com/adeo/summit/service/PDFVectorStore.java
@@ -2,10 +2,8 @@ package com.adeo.summit.service;
 
 import com.adeo.summit.config.ResourceProperties;
 import org.springframework.ai.document.Document;
-import org.springframework.ai.embedding.EmbeddingClient;
 import org.springframework.ai.reader.pdf.PagePdfDocumentReader;
 import org.springframework.ai.transformer.splitter.TokenTextSplitter;
-import org.springframework.ai.vectorstore.SimpleVectorStore;
 import org.springframework.ai.vectorstore.VectorStore;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Service;
@@ -18,13 +16,13 @@ public class PDFVectorStore {
 
     private final VectorStore vectorStore;
 
-    public PDFVectorStore(EmbeddingClient embeddingClient, ResourceProperties resourceProperties) {
-        this.vectorStore = new SimpleVectorStore(embeddingClient);
+    public PDFVectorStore(VectorStore vectorStore, ResourceProperties resourceProperties) {
+        this.vectorStore = vectorStore;
         for (Resource resource : resourceProperties.getResources()) {
             PagePdfDocumentReader reader = new PagePdfDocumentReader(resource);
             TokenTextSplitter splitter = new TokenTextSplitter();
             List<Document> documents = splitter.apply(reader.get());
-            this.vectorStore.accept(documents);
+            this.vectorStore.add(documents);
         }
     }
 
