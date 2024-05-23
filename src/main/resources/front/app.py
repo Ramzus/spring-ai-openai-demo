6e1850746e6652752590cab9213743f40f0de1ff
20014643 <marcel@marquez.fr>
2024-05-23 10:38:07 +0200

    fix chainlit code

--- src/main/resources/front/app.py
+++ src/main/resources/front/app.py
@@ -1,11 +1,14 @@
 import chainlit as cl
 import requests
 
+endpoint = 'http://localhost:8080/ai/callWithContext'
 
 @cl.on_message
 async def main(message: cl.Message):
-    # Send a response back to the useré
-    response = requests.get('http://localhost:8080/api/callWithContext', params=message)
+    # Send a response back to the user
+    requestsParam = {'message': requests.utils.quote(message.content)}
+    response = requests.get(endpoint, params=requestsParam)
+    responseContent = response.content.decode('utf-8')
     await cl.Message(
-        content=f"{message.content}",
+        content=f"{responseContent}",
     ).send()
