a5d822b02c91da6e464a3c951ee3213ac7017331
20014643 <marcel@marquez.fr>
2024-06-03 17:06:54 +0200

    upgrade to 1.0.0-SNAPSHOT

--- src/main/java/com/adeo/summit/service/PDFVectorStore.java
+++ src/main/java/com/adeo/summit/service/PDFVectorStore.java
@@ -11,7 +11,6 @@ import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.stream.Collectors;
 
 @Service
 public class PDFVectorStore {
